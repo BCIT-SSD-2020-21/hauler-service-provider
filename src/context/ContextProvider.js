502963fce335546e26a7a5452d10de467ea25f82
KulveerBrar <brarkulveer15@gmail.com>
2021-04-24 13:54:50 -0700

    add fn to get current user #9

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-
+import { auth } from "../api/firebase"
 
 export const Context = React.createContext();
 
@@ -10,11 +10,21 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState('');
 
+    // ==================================To get current user=================================//
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+        return unsubscribe
+    }, [])
+    
     return (
         <Context.Provider
             value={{
                 currentUser,
             }}>
+            {!loading && children}
         </Context.Provider>
     )
 }
