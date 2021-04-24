2da9a5971f158178ceb6f490e1d53f4f2ae71b44
KulveerBrar <brarkulveer15@gmail.com>
2021-04-24 02:44:26 -0700

    add model to edit profile, update README #7

--- src/screens/ProfileScreen/Profile.js
+++ src/screens/ProfileScreen/Profile.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react'
-import { Text, TextInput, TouchableOpacity, View, Image, ScrollView, Picker } from 'react-native'
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, Text, TouchableOpacity, View, ScrollView, Modal } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { FontAwesome } from '@expo/vector-icons';
+import UserInfo from '../../components/userInfo';
 
-export default function Signup() {
+export default function Profile() {
     const serviceProvider = {
-        email: "John@gmail.com",
-        dob: "1992/01/01",
-        address: "123 abc Street, Surrey, ABC 123",
-        phoneNumber: 1234567,
-        vehileType: "SUV",
-        flname: "John"
+        email: 'John@gmail.com',
+        dob: '1992/01/01',
+        address: '123 abc Street, Surrey, ABC 123',
+        phoneNumber: '1234567',
+        vehileType: 'SUV',
+        flname: 'John'
     }
+    const [modalVisible, setModalVisible] = useState(false);
+    const [dob, setDob] = useState(serviceProvider.dob)
+    const [address, setAddress] = useState(serviceProvider.address)
+    const [phoneNumber, setphoneNumber] = useState(serviceProvider.phoneNumber)
+    const [vehileType, setVehileType] = useState(serviceProvider.vehileType)
+    const [flname, setFlName] = useState(serviceProvider.flname)
+
     const onSignOutClicked = async () => {
-        console.log("user Logged out")
+        console.log('user Logged out')
     }
 
     const onEditClicked = async () => {
-        console.log("edit clicked")
+        setModalVisible(true)
+    }
+    const onEditSubmitted = async () => {
+        setModalVisible(!modalVisible)
     }
 
     return (
@@ -27,8 +37,8 @@ export default function Signup() {
                 <View style={styles.profileContainer}>
                     <View style={styles.avatar}>
                         <Avatar
-                            title="name"
-                            size="xlarge"
+                            title='name'
+                            size='xlarge'
                             rounded
                             source={{
                                 uri:
@@ -41,47 +51,78 @@ export default function Signup() {
                         {serviceProvider.flname}
                     </Text>
                     <View style={styles.headerContainer} >
-                        <FontAwesome name="star" size={20} color='#2EBCAC' />
-                        <FontAwesome name="star" size={20} color='#2EBCAC' />
-                        <FontAwesome name="star" size={20} color='#2EBCAC' />
-                        <FontAwesome name="star" size={20} color='#2EBCAC' />
-                        <FontAwesome name="star" size={20} color='#2EBCAC' />
+                        <FontAwesome name='star' size={20} color='#2EBCAC' />
+                        <FontAwesome name='star' size={20} color='#2EBCAC' />
+                        <FontAwesome name='star' size={20} color='#2EBCAC' />
+                        <FontAwesome name='star' size={20} color='#2EBCAC' />
+                        <FontAwesome name='star' size={20} color='#2EBCAC' />
                     </View>
                     <View style={styles.infoContainer}>
-                        <FontAwesome style={styles.infoIcon} name="user" size={24} color="black" />
+                        <FontAwesome style={styles.infoIcon} name='user' size={24} color='black' />
                         <Text style={styles.userInfo}>
                             {serviceProvider.flname}
                         </Text>
                     </View>
                     <View style={styles.infoContainer}>
-                        <FontAwesome style={styles.infoIcon} name="birthday-cake" size={24} color="black" />
+                        <FontAwesome style={styles.infoIcon} name='birthday-cake' size={24} color='black' />
                         <Text style={styles.userInfo}>
                             {serviceProvider.dob}
                         </Text>
                     </View>
                     <View style={styles.infoContainer}>
-                        <FontAwesome style={styles.infoIcon} name="envelope" size={24} color="black" />
+                        <FontAwesome style={styles.infoIcon} name='envelope' size={24} color='black' />
                         <Text style={styles.userInfo}>
                             {serviceProvider.email}
                         </Text>
                     </View>
                     <View style={styles.infoContainer}>
-                        <FontAwesome style={styles.infoIcon} name="phone" size={24} color="black" />
+                        <FontAwesome style={styles.infoIcon} name='phone' size={24} color='black' />
                         <Text style={styles.userInfo}>
                             {serviceProvider.phoneNumber}
                         </Text>
                     </View>
                     <View style={styles.infoContainer}>
-                        <FontAwesome style={styles.infoIcon} name="address-card" size={24} color="black" />
+                        <FontAwesome style={styles.infoIcon} name='address-card' size={24} color='black' />
                         <Text style={styles.userInfo}>
                             {serviceProvider.address}
                         </Text>
                     </View>
                     <View style={styles.infoContainer}>
-                        <FontAwesome style={styles.infoIcon} name="truck" size={24} color="black" />
+                        <FontAwesome style={styles.infoIcon} name='truck' size={24} color='black' />
                         <Text style={styles.userInfo}>
                             {serviceProvider.vehileType}
                         </Text>
+
+                        <Modal
+                            animationType='slide'
+                            transparent={false}
+                            opacity={0.5}
+                            visible={modalVisible}
+                            backdropOpacity={0.5}
+                            onRequestClose={() => {
+                                setModalVisible(!modalVisible);
+                            }}
+                        >
+                            <View style={styles.modalContainer}>
+                                <UserInfo
+                                    dob={dob}
+                                    address={address}
+                                    phoneNumber={phoneNumber}
+                                    vehileType={vehileType}
+                                    flname={flname}
+                                    setDob={setDob}
+                                    setAddress={setAddress}
+                                    setphoneNumber={setphoneNumber}
+                                    setVehileType={setVehileType}
+                                    setFlName={setFlName}
+                                />
+                                <TouchableOpacity
+                                    style={[styles.buttons, styles.logOutButton]}
+                                    onPress={() => onEditSubmitted()}>
+                                    <Text style={styles.buttonTitle}>Submit</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </Modal>
                     </View>
                 </View>
                 <View style={styles.buttonContainer}>
@@ -139,9 +180,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginVertical: 20,
     },
+    modalContainer: {
+        margin: 20
+    },
     buttonContainer: {
         flexDirection: 'row',
-        // justifyContent: 'space-between',
         marginVertical: 10,
     },
     buttons: {
@@ -149,7 +192,7 @@ const styles = StyleSheet.create({
         width: '50%',
         height: 48,
         borderRadius: 5,
-        alignItems: "center",
+        alignItems: 'center',
         justifyContent: 'center'
     },
     editButton: {
