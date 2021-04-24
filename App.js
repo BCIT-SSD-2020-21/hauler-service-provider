89221042e8cc3aae40de7db8215d1a39c9424970
KulveerBrar <brarkulveer15@gmail.com>
2021-04-24 16:44:24 -0700

    Home screen added #14

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import Signup from './src/screens/SignUpScreen/SignUp';
 import Signin from './src/screens/SignInScreen/Signin';
 import Profile from './src/screens/ProfileScreen/Profile';
 import { AuthProvider } from './src/context/ContextProvider';
+import Home from './src/screens/HomeScreen/Home';
 
 export default function App() {
   return (
     <AuthProvider>
       <View>
         {/* <Signup /> */}
-        <Signin />
+        {/* <Signin /> */}
         {/* <Profile /> */}
+        <Home />
       </View>
     </AuthProvider>
   );
