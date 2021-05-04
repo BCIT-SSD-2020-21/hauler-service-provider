e8db763ab83175e560b1d78495089da10d65e4ee
KulveerBrar <brarkulveer15@gmail.com>
2021-05-03 22:53:38 -0700

    Render post details on confirmation screen #28

--- src/screens/JobConfirmationScreen/JobConfirmation.js
+++ src/screens/JobConfirmationScreen/JobConfirmation.js
@@ -2,72 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import PostInfo from '../../components/postInfo/PostInfo';
 
-export default function JobConfirmation({navigation}) {
-    const posts = {
-        userId: "hbsdhfvajfhasmdf",
-        _id: '1234',
-        service: 'Junk Removal',
-        postHeading: 'Bed, Sofa and Tv Stand to dispose',
-        postDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        loadWeight: "Medium",
-        numberOfItems: "4",
-        loadImages: [{
-            _id: '1',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '2',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '3',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '4',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '5',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '6',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '7',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '8',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '9',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        },
-        {
-            _id: '10',
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        }],
-        pickUpProvince: "BC",
-        pickUpCity: "Surrey",
-        pickUpStreetAddress: "123 abc Street",
-        pickUpZipCode: "AB1 12C",
-        pickUpContactPerson: "John",
-        pickUpContactNumber: "64 1234567",
-        pickUpSpecialInstruction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        dropOffProvince: "BC",
-        dropOffCity: "Hope",
-        dropOffStreetAddress: "234 defg Street",
-        dropOffZipCode: "V3W 12D",
-        dropOffContactPerson: "Sam",
-        dropOffContactNumber: "64 3456789",
-        dropOffSpecialInstruction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-        price: 60
-    }
-
+export default function JobConfirmation({navigation, route}) {
+    const { posts } = route.params;
     const onJobListPressed = () => {
         navigation.navigate('MyJobListNavigator')
     }
@@ -98,7 +34,6 @@ export default function JobConfirmation({navigation}) {
                 </TouchableOpacity>
             </View>
         </ScrollView>
-
     )
 }
 
--- src/screens/PostDetailScreen/PostDetails.js
+++ src/screens/PostDetailScreen/PostDetails.js
@@ -4,75 +4,10 @@ import { getOnePost } from '../../../network';
 import PostInfo from '../../components/postInfo/PostInfo';
 
 export default function PostDetails({navigation, route }) {
-    // const posts = {
-    //     userId: "hbsdhfvajfhasmdf",
-    //     _id: '1234',
-    //     service: 'Junk Removal',
-    //     postHeading: 'Bed, Sofa and Tv Stand to dispose',
-    //     postDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    //     loadWeight: "Medium",
-    //     numberOfItems: "4",
-    //     loadImages: [{
-    //         _id: '1',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '2',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '3',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '4',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '5',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '6',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '7',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '8',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '9',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     },
-    //     {
-    //         _id: '10',
-    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-    //     }],
-    //     pickUpProvince: "BC",
-    //     pickUpCity: "Surrey",
-    //     pickUpStreetAddress: "123 abc Street",
-    //     pickUpZipCode: "AB1 12C",
-    //     pickUpContactPerson: "John",
-    //     pickUpContactNumber: "64 1234567",
-    //     pickUpSpecialInstruction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    //     dropOffProvince: "BC",
-    //     dropOffCity: "Hope",
-    //     dropOffStreetAddress: "234 defg Street",
-    //     dropOffZipCode: "V3W 12D",
-    //     dropOffContactPerson: "Sam",
-    //     dropOffContactNumber: "64 3456789",
-    //     dropOffSpecialInstruction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    //     price: 60
-    // }
-
     const [posts, setPosts] = useState('')
     const { postId } = route.params;
     const onAccept = () => {
-        navigation.navigate('JobConfirmation')
+        navigation.navigate('JobConfirmation', {posts:posts})
     }
 
     const onOffer = () => {
