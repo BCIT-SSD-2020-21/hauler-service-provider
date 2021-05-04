71b425c290bcc60d6df74e9a29f60e65d8b1895e
KulveerBrar <brarkulveer15@gmail.com>
2021-05-03 23:42:20 -0700

    Render posts on MyJobList screen #28

--- src/screens/MyJobListScreen/MyJobList.js
+++ src/screens/MyJobListScreen/MyJobList.js
@@ -1,47 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import SearchByService from '../../components/searchByService/SearchByService';
 import SearchByLocation from '../../components/searchByLocation/SearchByLocation';
 import PostsList from '../../components/postList/PostsList';
+import { getOnePost } from '../../../network';
 
 export default function MyJobList({navigation}) {
-    const posts = [{
-        _id: '1234',
-        service: 'Junk Removal',
-        postHeading: 'Bed, Sofa and Tv Stand to dispose',
-        postDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        pickUpCity: 'Surrey',
-        pickUpProvince: 'BC',
-        loadImages: [{
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        }],
-    },
-    {
-        _id: '12345',
-        service: 'Moving',
-        postHeading: 'Bed, Sofa and Tv Stand to move',
-        postDescription: ' MovingLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        pickUpCity: 'Surrey',
-        pickUpProvince: 'BC',
-        dropOffCity: 'Hope',
-        dropOffProvince: 'BC',
-        loadImages: [{
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        }],
-    },
-    {
-        _id: '123456',
-        service: 'Moving',
-        postHeading: 'Bed, Sofa and Tv Stand to move',
-        postDescription: ' MovingLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        pickUpCity: 'Surrey',
-        pickUpProvince: 'BC',
-        dropOffCity: 'Hope',
-        dropOffProvince: 'BC',
-        loadImages: [{
-            imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
-        }],
-    }]
+
+const postIds= ["608fba4ddc55d161f4e89889","608fc06d2113ac6cc0ef7ba4"]
+const [posts, setPosts] = useState('')
+
+    // const posts = [{
+    //     _id: '1234',
+    //     service: 'Junk Removal',
+    //     postHeading: 'Bed, Sofa and Tv Stand to dispose',
+    //     postDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //     pickUpCity: 'Surrey',
+    //     pickUpProvince: 'BC',
+    //     loadImages: [{
+    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
+    //     }],
+    // },
+    // {
+    //     _id: '12345',
+    //     service: 'Moving',
+    //     postHeading: 'Bed, Sofa and Tv Stand to move',
+    //     postDescription: ' MovingLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //     pickUpCity: 'Surrey',
+    //     pickUpProvince: 'BC',
+    //     dropOffCity: 'Hope',
+    //     dropOffProvince: 'BC',
+    //     loadImages: [{
+    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
+    //     }],
+    // },
+    // {
+    //     _id: '123456',
+    //     service: 'Moving',
+    //     postHeading: 'Bed, Sofa and Tv Stand to move',
+    //     postDescription: ' MovingLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //     pickUpCity: 'Surrey',
+    //     pickUpProvince: 'BC',
+    //     dropOffCity: 'Hope',
+    //     dropOffProvince: 'BC',
+    //     loadImages: [{
+    //         imageUrl: 'https://samsjunkremoval.ca/wp-content/uploads/2013/04/158-660x371.jpg'
+    //     }],
+    // }]
 
     const ServiceProviderAction = [{
         _id: 'abc',
@@ -79,12 +84,26 @@ export default function MyJobList({navigation}) {
     const [location, setLocation] = useState('')
     const [service, setService] = useState('')
 
-    const onViewDetailsPress = () => {
-        navigation.navigate('PostDetails')
+    const onViewDetailsPress = (value) => {
+        navigation.navigate('PostDetails', {postId:value.postId})
     }
     const onStatusDeailsPress = () => {
         navigation.navigate('StatusDetails')
     }
+
+    useEffect(() => {
+        (async () => {
+           const posts = await Promise.all(postIds.map(async (a) => {
+            if (!!a) {
+              return await getOnePost(a);
+            } else { return null }
+          }))
+          setPosts(posts)
+
+            // const newPosts = await getAllPosts()
+            // setPosts(newPosts)
+        })()
+    }, [])
 
     return (
         <View style={styles.container}>
