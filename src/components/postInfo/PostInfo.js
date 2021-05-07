896778322d835113834906017857d7bf495fcb46
KulveerBrar <brarkulveer15@gmail.com>
2021-05-06 21:13:03 -0700

    Replace list with flatlist #36

--- src/components/postInfo/PostInfo.js
+++ src/components/postInfo/PostInfo.js
@@ -31,20 +31,19 @@ export default function PostInfo({ posts }) {
                 <Text style={styles.infoKey}>Pick Up Instructions</Text>
                 <Text style={styles.infoValue}>{posts.pickUpSpecialInstruction}</Text>
             </View>
-
             {posts.dropOffStreetAddress &&
-            <View>
-                <View style={styles.infoContainer}>
-                    <Text style={styles.infoKey}>Drop Off Address</Text>
-                    <Text style={styles.infoValue}>
-                        {posts.dropOffStreetAddress}, {posts.dropOffCity}, {posts.dropOffProvince}, {posts.dropOffZipCode}
-                    </Text>
+                <View>
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.infoKey}>Drop Off Address</Text>
+                        <Text style={styles.infoValue}>
+                            {posts.dropOffStreetAddress}, {posts.dropOffCity}, {posts.dropOffProvince}, {posts.dropOffZipCode}
+                        </Text>
+                    </View>
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.infoKey}>Drop Off Instructions</Text>
+                        <Text style={styles.infoValue}>{posts.dropOffSpecialInstruction}</Text>
+                    </View>
                 </View>
-                <View style={styles.infoContainer}>
-                <Text style={styles.infoKey}>Drop Off Instructions</Text>
-                <Text style={styles.infoValue}>{posts.dropOffSpecialInstruction}</Text>
-            </View>
-            </View>
             }
             <View style={styles.imageContainer}>
                 <Text style={styles.infoKey}>Images</Text>
@@ -79,42 +78,5 @@ const styles = StyleSheet.create({
         marginRight: 10,
         width: '65%'
     },
-
-
-
-
-
-    addressSubContainer: {
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 8,
-        width: '90%'
-    },
-    addressContainer: {
-        alignItems: 'center',
-    },
-    heading: {
-        borderBottomColor: 'black',
-        borderBottomWidth: 2,
-        textAlign: 'center',
-        marginBottom: 10,
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    description: {
-        overflow: 'hidden',
-        borderBottomColor: 'black',
-        borderBottomWidth: 2,
-        paddingBottom: 10,
-    },
-    measurementsContainer: {
-        alignItems: 'center',
-        marginVertical: 10
-    },
-    detailsContainer: {
-        width: '90%'
-    },
-
 })
 
