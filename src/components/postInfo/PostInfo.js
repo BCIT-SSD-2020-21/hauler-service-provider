7efdb7232f9f9050e3fa44a708022b770621b8d6
KulveerBrar <brarkulveer15@gmail.com>
2021-05-06 21:05:58 -0700

    Add styling to post details screen #36

--- src/components/postInfo/PostInfo.js
+++ src/components/postInfo/PostInfo.js
@@ -4,53 +4,86 @@ import ImageList from '../imageList/ImageList';
 
 export default function PostInfo({ posts }) {
     return (
-        <View>
-            <View style={styles.detailsContainer}>
-                <Text style={styles.heading}>{posts.postHeading}</Text>
-                <Text style={styles.description}>{posts.postDescription}</Text>
-                <View style={styles.measurementsContainer}>
-                    <Text>Weight: {posts.loadWeight}</Text>
-                    <Text>Number of items: {posts.numberOfItems}</Text>
-                </View>
+        <View style={styles.container}>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Post Heading</Text>
+                <Text style={styles.infoValue}>{posts.postHeading}</Text>
             </View>
-            <Text>
-            {posts &&
-                <ImageList
-                    loadImages={posts.loadImages}
-                />
-            }
-            </Text>
-            <View style={styles.addressContainer}>
-                <Text>Pick Up Address</Text>
-                <View style={styles.addressSubContainer}>
-                    <Text>
-                        {posts.pickUpStreetAddress}
-                    </Text>
-                    <Text>
-                        {posts.pickUpCity} {posts.pickUpProvince} {posts.pickUpZipCode}
-                    </Text>
-                </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Post Description</Text>
+                <Text style={styles.infoValue}>{posts.postDescription}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Total Weight</Text>
+                <Text style={styles.infoValue}>{posts.loadWeight}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Number of items </Text>
+                <Text style={styles.infoValue}>{posts.numberOfItems}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Pick Up Address</Text>
+                <Text style={styles.infoValue}>
+                    {posts.pickUpStreetAddress}, {posts.pickUpCity}, {posts.pickUpProvince}, {posts.pickUpZipCode}
+                </Text>
+            </View>
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Pick Up Instructions</Text>
+                <Text style={styles.infoValue}>{posts.pickUpSpecialInstruction}</Text>
             </View>
 
             {posts.dropOffStreetAddress &&
-                <View style={styles.addressContainer}>
-                    <Text>Drop Off Address</Text>
-                    <View style={styles.addressSubContainer}>
-                        <Text>
-                            {posts.dropOffStreetAddress}
-                        </Text>
-                        <Text>
-                            {posts.dropOffCity} {posts.dropOffProvince} {posts.dropOffZipCode}
-                        </Text>
-                    </View>
+            <View>
+                <View style={styles.infoContainer}>
+                    <Text style={styles.infoKey}>Drop Off Address</Text>
+                    <Text style={styles.infoValue}>
+                        {posts.dropOffStreetAddress}, {posts.dropOffCity}, {posts.dropOffProvince}, {posts.dropOffZipCode}
+                    </Text>
                 </View>
+                <View style={styles.infoContainer}>
+                <Text style={styles.infoKey}>Drop Off Instructions</Text>
+                <Text style={styles.infoValue}>{posts.dropOffSpecialInstruction}</Text>
+            </View>
+            </View>
             }
+            <View style={styles.imageContainer}>
+                <Text style={styles.infoKey}>Images</Text>
+                <Text>
+                    {posts &&
+                        <ImageList
+                            loadImages={posts.loadImages}
+                        />
+                    }
+                </Text>
+            </View>
         </View>
-
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        width: '95%',
+    },
+    infoContainer: {
+        flexDirection: 'row',
+        marginVertical: 10
+    },
+    imageContainer: {
+        marginVertical: 10
+    },
+    infoKey: {
+        color: '#A9A9A9',
+        width: 140,
+    },
+    infoValue: {
+        marginRight: 10,
+        width: '65%'
+    },
+
+
+
+
+
     addressSubContainer: {
         padding: 10,
         borderWidth: 1,
@@ -70,6 +103,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     description: {
+        overflow: 'hidden',
         borderBottomColor: 'black',
         borderBottomWidth: 2,
         paddingBottom: 10,
--- src/screens/PostDetailScreen/PostDetails.js
+++ src/screens/PostDetailScreen/PostDetails.js
@@ -27,42 +27,63 @@ export default function PostDetails({navigation, route }) {
                 <PostInfo
                     posts={posts}
                 />
-                <Text>Price: ${posts.price}</Text>
+                <View style={styles.priceContainer}>
+                <Text style={styles.infoKey}>Quoted Price</Text>
+                <Text style={styles.infoValue}> ${posts.price}</Text>
+                </View>
                 <TouchableOpacity
-                    style={styles.button}
+                    style={[styles.button, styles.acceptButton]}
                     onPress={() => onAccept()}>
                     <Text style={styles.buttonTitle}>ACCEPT</Text>
                 </TouchableOpacity>
-                
                 <TouchableOpacity
-                    style={styles.button}
+                    style={[styles.button,styles.offerButton]}
                     onPress={() => onOffer()}>
                     <Text style={styles.buttonTitle}>OFFER</Text>
                 </TouchableOpacity>
             </View>
         </ScrollView>
-
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor:'white',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'flex-start',
+        justifyContent: 'center',
         paddingVertical: '2%',
         width: '100%',
         height: '100%'
     },
-
+    priceContainer:{
+        width: '95%',
+        flexDirection:'row',
+        marginVertical: 10
+    },
+    infoKey: {
+        color: '#A9A9A9',
+        width: 140,
+    },
+    infoValue: {
+        overflow: 'hidden',
+        marginRight: 10,
+        width: '65%'
+    },
     button: {
-        backgroundColor: 'black',
         marginVertical: 10,
         height: 48,
-        borderRadius: 5,
+        borderRadius: 10,
         alignItems: 'center',
         justifyContent: 'center',
-        width: 100
+        width: '95%'
+    },
+    acceptButton:{
+        
+        backgroundColor: '#A9A9A9',
+    },
+    offerButton:{
+        backgroundColor: '#0077FC',
     },
     buttonTitle: {
         color: 'white',
